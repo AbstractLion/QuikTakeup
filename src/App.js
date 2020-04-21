e9146a9ff14398c4dc5d9f62c920adc2914b64d5
AbstractUltra <dragonhe36@gmail.com>
2020-04-20 23:21:34 -0400

    3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:4001";
+
+const socket = socketIOClient(ENDPOINT);
 
 function App() {
+  const [response, setResponse] = useState([]);
+  let claimedQuestions = [];
+  socket.on("questionUpdate", (data) => {
+    setResponse(data);
+  });
+  function claimQuestion(questionID) {
+    socket.emit("questionClaim", questionID);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {response.map((question) => {
+        return (
+          <div>
+            <p>{question.question}</p>
+            {!question.claimed ? (
+              <a href="#" onClick={() => claimQuestion(question.id)}>
+                Claim Question
+              </a>
+            ) : (
+              <p>Claimed by {question.claimed}</p>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
