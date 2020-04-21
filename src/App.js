c07c0315eabd0e6434d8350704dc4f3134362157
AbstractUltra <dragonhe36@gmail.com>
2020-04-21 09:03:16 -0400

    allow student custom name

--- src/App.js
+++ src/App.js
@@ -8,21 +8,31 @@ const socket = socketIOClient(ENDPOINT);
 
 function App() {
   const [response, setResponse] = useState([]);
+  const [studentName, setStudentName] = useState("Leon");
   let claimedQuestions = [];
   socket.on("questionUpdate", (data) => {
     setResponse(data);
   });
   function claimQuestion(questionID) {
-    socket.emit("questionClaim", questionID);
+    socket.emit("questionClaim", {id: questionID, name: studentName});
+  }
+  function handleNameChange(event) {
+    setStudentName(event.target.value);
   }
   return (
     <div className="App">
+      <textarea onChange={handleNameChange}></textarea>
       {response.map((question) => {
         return (
           <div>
             <p>{question.question}</p>
             {!question.claimed ? (
-              <a href="#" onClick={() => claimQuestion(question.id)}>
+              <a
+                href="#"
+                onClick={() => {
+                  claimQuestion(question.id);
+                }}
+              >
                 Claim Question
               </a>
             ) : (
